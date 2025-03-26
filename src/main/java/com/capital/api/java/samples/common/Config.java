42c6326b4d5af72c11ca92a2c6f75f2e41e88133
Murawski Łukasz <Lukasz.Murawski@generali.com>
2025-03-26 20:30:33 +0100

    logbook

--- src/main/java/com/capital/api/java/samples/common/Config.java
+++ src/main/java/com/capital/api/java/samples/common/Config.java
@@ -5,12 +5,16 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
 @Configuration
 public class Config {
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+    public RestTemplate restTemplate(RestTemplateBuilder builder, Logbook logbook) {
+        return builder
+//                       .additionalInterceptors(new LogbookClientHttpRequestInterceptor(logbook))
+                       .build();
     }
 
     @Bean
